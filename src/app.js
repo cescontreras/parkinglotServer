c95d16dddad9a88372620636329ae1bfb0565c9f
cescontreras <contreras-cesaralejandro@hotmail.com>
2021-01-07 23:27:09 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -5,11 +5,6 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const server = express();
 
-// server.use(cors({
-//   origin: "https://henry-comics.vercel.app",
-//   credentials: true,
-// }));
-
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
